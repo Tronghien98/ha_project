a056f652e4267452e94e27bbf3a03faccd6cbd16
Quyquai <quyquai96@gmail.com>
2023-11-24 23:12:26 +0700

    done table

--- ha_project_fe/src/pages/admin/Categories.js
+++ ha_project_fe/src/pages/admin/Categories.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import styles from "../../styles/admin/categories.module.css";
 
 import HeaderAdmin from "../../components/admin/Header";
@@ -43,19 +44,43 @@ class Categories extends Component {
                     <tr>
                       <th className={styles.colTableNum}>#</th>
                       <th>Tên danh mục</th>
-                      <th>Xử lý</th>
+                      <th className={styles.colTableAction}>Xử lý</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td className={styles.colTableNum}>1</td>
                       <td>Danh mục 1</td>
-                      <td></td>
+                      <td>
+                        <div>
+                          <Button color="danger" className="mx-1 py-0">
+                            Xóa
+                          </Button>
+                          <Button color="warning" className="mx-1 py-0">
+                            Sửa
+                          </Button>
+                          <div className={styles.btnUpDown}>
+                            <AiFillCaretUp />
+                            <AiFillCaretDown />
+                          </div>
+                        </div>
+                      </td>
                     </tr>
                     <tr>
                       <td className={styles.colTableNum}>2</td>
                       <td>Danh mục 2</td>
-                      <td></td>
+                      <td>
+                        <div>
+                          <Button color="danger" className="mx-1 py-0">
+                            Xóa
+                          </Button>
+                          <Button color="warning" className="mx-1 py-0">
+                            Sửa
+                          </Button>
+                          <AiFillCaretUp />
+                          <AiFillCaretDown />
+                        </div>
+                      </td>
                     </tr>
                   </tbody>
                 </Table>
